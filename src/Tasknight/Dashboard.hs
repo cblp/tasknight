49700fb502398b041c08cc705905bdee8d51362f
Yuriy Syrovetskiy <cblp@cblp.su>
2016-04-03 14:48:37 +0300

    [+] show dashboard with YAML

--- src/Tasknight/Dashboard.hs
+++ src/Tasknight/Dashboard.hs
@@ -2,15 +2,26 @@
 
 module Tasknight.Dashboard (mainWith) where
 
-import Control.Error (runScript)
-import Control.Monad.IO.Class (liftIO)
-import Data.Foldable (for_)
-import Data.Traversable (for)
+import            Control.Error (runScript)
+import            Control.Monad.Trans.Class (lift)
+import            Data.ByteString (ByteString)
+import qualified  Data.ByteString.Char8 as ByteString
+import qualified  Data.Text as Text
+import            Data.Traversable (for)
+import qualified  Data.Yaml as Yaml
+import            Data.Yaml ((.=), object)
 
 import Tasknight.Dashboard.Config (Config(..))
-import Tasknight.Provider (Provider(..))
+import Tasknight.Provider (Item(..), ItemList(..), Provider(..))
 
 mainWith :: Config -> IO ()
 mainWith Config{providers} = runScript $ do
     lists <- mconcat <$> for providers getLists
-    for_ lists $ liftIO . print
+    lift . ByteString.putStrLn $ showDashboard lists
+
+showDashboard :: [ItemList] -> ByteString
+showDashboard lists =
+    Yaml.encode $ object
+        [ Text.pack name .= [Text.pack item | Item item <- items]
+        | ItemList{name, items} <- lists
+        ]
--- src/Tasknight/Provider.hs
+++ src/Tasknight/Provider.hs
@@ -3,9 +3,7 @@ module Tasknight.Provider (Item(..), ItemList(..), Provider(..)) where
 import Control.Error (Script)
 
 data Item = Item String
-    deriving Show
 
 data ItemList = ItemList { name :: String, items :: [Item] }
-    deriving Show
 
 data Provider = Provider { getLists :: Script [ItemList] }
