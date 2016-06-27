a8484d3f9f84c83edeb7c9b86a3281eefabce097
Yuriy Syrovetskiy <cblp@cblp.su>
2016-06-27 13:05:02 +0300

    dashboard: [+] put current date in footer

--- dashboard/src/Tasknight/Dashboard.hs
+++ dashboard/src/Tasknight/Dashboard.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Tasknight.Dashboard (mainWith, showDashboard) where
 
@@ -6,9 +7,12 @@ import           Control.Error              (runScript)
 import           Control.Monad.Trans.Class  (lift)
 import           Data.ByteString.Lazy       (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as ByteString
+import           Data.Foldable              (for_)
+import           Data.Monoid                ((<>))
 import qualified Data.Text                  as Text
+import           Data.Time                  (UTCTime, getCurrentTime)
 import           Data.Traversable           (for)
-import           Lucid                      (a_, body_, h1_, href_, html_, li_, ol_, renderBS,
+import           Lucid                      (a_, body_, h1_, href_, html_, li_, ol_, p_, renderBS,
                                              toHtml)
 
 import Tasknight.Dashboard.Config (Config(..))
@@ -17,14 +21,16 @@ import Tasknight.Provider         (Item(..), ItemList(..), Provider(..))
 mainWith :: Config -> IO ()
 mainWith Config{providers} = runScript $ do
     lists <- mconcat <$> for providers getLists
-    lift . ByteString.putStrLn $ showDashboard lists
+    updateTime <- lift getCurrentTime
+    lift . ByteString.putStrLn $ showDashboard updateTime lists
 
-showDashboard :: [ItemList] -> ByteString
-showDashboard lists = renderBS . html_ . body_ $ mconcat
-    [ do  h1_ $ toHtml name
-          ol_ $ mconcat
-              [ li_ . a_ [href_ $ toText uri] $ toHtml text | Item{text, uri} <- items ]
-    | ItemList{name, items} <- lists
-    ]
+showDashboard :: UTCTime -> [ItemList] -> ByteString
+showDashboard updateTime lists = renderBS . html_ . body_ $ do
+    for_ lists $ \ItemList{name, items} -> do
+        h1_ $ toHtml name
+        ol_ .
+            for_ items $ \Item{text, uri} ->
+                li_ . a_ [href_ $ toText uri] $ toHtml text
+    p_ . toHtml $ "Updated at " <> show updateTime
   where
     toText = Text.pack . show
