84aebd9f27a4403c521c05e78737bcff6c0b4b1f
Yuriy Syrovetskiy <cblp@cblp.su>
2016-07-23 12:08:15 +0200

    [+] Postgres config for Travis

--- db-devel-init.sh
+++ db-devel-init.sh
@@ -4,6 +4,9 @@ set -eux -o pipefail
 if [[ `uname -s` == Darwin ]]; then
     # assume brew
     psql_cmd="psql postgres"
+elif [[ ${TRAVIS:-false} == true ]]; then
+    # Travis CI
+    psql_cmd="psql --username=postgres"
 else
     # assume ubuntu
     psql_cmd="sudo -u postgres psql"
